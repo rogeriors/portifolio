92ad0ec4f3c83a363a2bc820470c12ff1da76f51
Rogério Rodrigues dos Santos <rogerio_r_s@hotmail.com.br>
2025-12-05 16:15:02 -0300

    update

--- scrips/script.js
+++ scrips/script.js
@@ -1,3 +1,23 @@
+// Espera 2000 milissegundos (2 segundos)
+setTimeout(function() {
+    const splashScreen = document.getElementById('splash-screen');
+    const mainContent = document.getElementById('main-content');
+
+    // Opcional: Adiciona a classe fade-out para um efeito suave
+    splashScreen.classList.add('fade-out');
+    document.body.style.backgroundColor = 'black';
+
+    // Espera o fade-out terminar (0.5s) para esconder os elementos completamente
+    setTimeout(function() {
+        splashScreen.style.display = 'none';
+        mainContent.style.display = 'block'; // Mostra o conteúdo principal
+        
+
+        // Restaura a rolagem da página
+        document.body.style.overflow = 'auto';
+    }, 500); // 500ms é a duração da transição CSS
+}, 1000);
+
 function atualizarRelogio() {
     const relogioElemento = document.getElementById('relogio'); // Seleciona a div do relógio
     const agora = new Date(); // Pega a data e hora atual
